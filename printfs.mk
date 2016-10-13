55202c801e870be64485f3afb120652ca02b0d15
computermouth <computermouth@crunchbangplusplus.org>
2016-10-12 19:19:12 -0700

    initial commit

---
+++ printfs.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# printfs
+#
+################################################################################
+
+PRINTFS_VERSION = master
+PRINTFS_REPO_NAME = printfs
+PRINTFS_SITE = https://github.com/computermouth/$(PRINTFS_REPO_NAME)
+PRINTFS_SITE_METHOD = git
+
+define PRINTFS_BUILD_CMDS
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define PRINTFS_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(@D)printfs $(TARGET_DIR)/usr/bin/printfs
+endef
+
+$(eval $(generic-package))
+
